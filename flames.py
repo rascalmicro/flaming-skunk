c6d70489437298deda01ed41db7a88529f515c27
Brandon Stafford <brandon@pingswept.org>
2016-02-09 18:53:03 +0000

    Add code to read Lots Of Pots board; remove some cruft

--- flames.py
+++ flames.py
@@ -2,13 +2,33 @@
 
 # Light each LED in sequence, and repeat.
 
-import colorsys, opc, subprocess, time
+import colorsys
+import opc
+import spidev
+import subprocess
+import time
 from PIL import Image
 
 numLEDs = 64
+
+def readadc(channel):
+    if channel > 7 or channel < 0:
+        return -1
+    # spi.xfer2 sends three bytes and returns three bytes:
+    # byte 1: the start bit (always 0x01)
+    # byte 2: configure bits, see MCP3008 datasheet table 5-2
+    # byte 3: don't care
+    r = spi.xfer2([1, 8 + channel << 4, 0])
+    # Three bytes are returned; the data (0-1023) is in the
+    # lower 3 bits of byte 2, and byte 3 (datasheet figure 6-1)
+    v = ((r[1] & 3) << 8) + r[2]
+    return v;
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+
 client = opc.Client('localhost:7890')
 
-#im = Image.open('flames.jpg')
 gif_im = Image.open('embers-2015-04-14.gif')
 
 im = gif_im.convert('RGB')
@@ -18,27 +38,11 @@ spacing = width / numLEDs
 
 pixels = [ (0,0,0) ] * numLEDs
 
-from socket import *
-s = socket(AF_INET, SOCK_DGRAM)
-s.bind(("127.0.0.1",10000))
-
 while True:
     for row in range(height):
-#        try:
-            # readings = subprocess.check_output('read-adc')
-            # shift = float(int(readings.split('\n')[4].split(' ')[2], 16))/65536.0
-#        f = open('/var/www/public/color.txt', 'r')
-#        color = f.read()
-#        f.close()
-#        (red,green,blue) = color.strip().split(',')
-#        shift, nothing, whatever = colorsys.rgb_to_hsv(float(red)/255.0, float(green)/255.0, float(blue)/255.0)
-        data, addr = s.recvfrom(100)
         # data should be in the format: shift, fire/nofire
-        shift = float(data.split(",")[0]) #float(bbio.analogRead(bbio.AIN1))/1400.0
-        print('Shift is {0}'.format(shift))
-     #       except:
-     #       shift = 0.0
-     #       print('Shift is {0}'.format(shift))
+        shift = float(readadc(0))/1023.0
+#        print('Shift is {0}'.format(shift))
         for i in range(numLEDs):
             r, g, b = im.getpixel((i * spacing, row))
             h, s, v = colorsys.rgb_to_hsv(r/255.0, g/255.0, b/255.0)
@@ -46,10 +50,3 @@ while True:
             r, g, b = colorsys.hsv_to_rgb(h, s, v)
             pixels[i] = r*255.0, g*255.0, b*255.0
         client.put_pixels(pixels)
-#        time.sleep(0.01)
-
-#	for i in range(numLEDs):
-#		pixels = [ (0,0,0) ] * numLEDs
-#		pixels[i] = (255, 255, 255)
-#		client.put_pixels(pixels)
-#		time.sleep(0.01)
