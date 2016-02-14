14dee60f0fb51f46fab7ffb00a1634cd89b695b4
Brandon Stafford <brandon@pingswept.org>
2016-02-14 19:52:19 +0000

    Hey, it sort of works!

--- flames.py
+++ flames.py
@@ -19,8 +19,6 @@ def setupSpiPins(clkPin, misoPin, mosiPin, csPin):
 def transfer_byte(data, clkPin, misoPin, mosiPin):
     retVal = 0
 
-#    print data
-
     for bit in range(8):
         # Set RPi's output bit high or low depending on highest bit of data field
         if data & 0x80:
@@ -42,12 +40,10 @@ def transfer_byte(data, clkPin, misoPin, mosiPin):
         GPIO.output(clkPin, GPIO.HIGH)
         GPIO.output(clkPin, GPIO.LOW)
 
-#    print retVal
     return retVal/2
 
 def transfer_bytes(bytes, clkPin, misoPin, mosiPin, csPin):
 
-    # data <<= (8-numBits)
     received_bytes = [transfer_byte(byte, clkPin, misoPin, mosiPin) for byte in bytes]
     return received_bytes
 
@@ -68,8 +64,14 @@ def readADC(channel, clkPin, misoPin, mosiPin, csPin):
 
     return float(((reading[1] & 3) << 8) + reading[2])/1023.0
 
+RED_BUTTON = 17
+BLUE_BUTTON = 18
+YELLOW_BUTTON = 27
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(RED_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BLUE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(YELLOW_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 numLEDs = 64
 
@@ -86,23 +88,50 @@ spacing = width / numLEDs
 
 pixels = [ (0,0,0) ] * numLEDs
 
+iterations = 0
+
+# hacky code to calculate average values of flickery flames image
+#
+#red = 0.0
+#green = 0.0
+#blue = 0.0
+
+#for row in range(height):
+#    for i in range(numLEDs):
+#        r, g, b = im.getpixel((i * spacing, row))
+#        red = red + float(r)
+#        green = green + float(g)
+#        blue = blue + float(b)
+
+#print red/(height * numLEDs)
+#print green/(height * numLEDs)
+#print blue/(height * numLEDs)
+
 while True:
     for row in range(height):
 
-        if(GPIO.input(17) !=1):
-            print("Button 1 pressed")
-        else:
-            print("nothing")
-
-        # data should be in the format: shift, fire/nofire
         hue_shift = readADC(0, 11, 9, 10, 8)
-#        print('Shift is {0}'.format(shift))
         brightness = readADC(1, 11, 9, 10, 8)
+        duration = readADC(2, 11, 9, 10, 8)
+        #print "duration: {0}".format(duration)
         for i in range(numLEDs):
-            r, g, b = im.getpixel((i * spacing, row))
+            # Flicker if blue button pressed, or red pressed recently
+            if((GPIO.input(BLUE_BUTTON) !=1) or (iterations > 0)):
+                r, g, b = im.getpixel((i * spacing, row))
+            else:
+                r, g, b = (152, 48, 13) # assign average values of flames image
+            # If red button pressed, start long period of flickering
+            if(GPIO.input(RED_BUTTON) != 1):
+                iterations = int(duration * 100)
+            # If yellow button pressed, stop flickering, even if in long period
+            if(GPIO.input(YELLOW_BUTTON) != 1):
+                iterations = 0
+                r, g, b = (152, 48, 13) # assign average values of flames image
             h, s, v = colorsys.rgb_to_hsv(r/255.0, g/255.0, b/255.0)
             h = (h + hue_shift) % 1
             v = v * brightness
             r, g, b = colorsys.hsv_to_rgb(h, s, v)
             pixels[i] = r*255.0, g*255.0, b*255.0
         client.put_pixels(pixels)
+        #print iterations
+    iterations = iterations - 1
